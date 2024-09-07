99ad9578cb09a4484c936af8071f767377d74e96
imCyberMohsin <mohsinansari7033@gmail.com>
2024-09-07 21:29:40 +0530

    updated

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useConversation from '../../zustand/useConversation'
 import { useSocketContext } from '../../context/SocketContext';
 
-const Conversation = ({ conversation, emoji, lastIndex, }) => {
-    // console.log(conversation);
-
+const Conversation = ({ conversation, emoji, lastIndex }) => {
     const { selectedConversation, setSelectedConversation } = useConversation();
     const isSelected = selectedConversation?._id === conversation._id;
     const { onlineUsers } = useSocketContext();
     const isOnline = onlineUsers.includes(conversation._id);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    };
+
     return (
         <>
-            <div className={`flex gap-2 items-center hover:bg-green-500 rounded p-3 py-1 cursor-pointer transition-all ${isSelected ? "bg-green-500" : ""}`}
+            <div
+                className={`flex gap-2 items-center hover:bg-green-500 rounded p-3 py-1 cursor-pointer transition-all ${isSelected ? "bg-green-500" : ""}`}
                 onClick={() => setSelectedConversation(conversation)}
             >
-                <div className={`avatar ${isOnline ? "online" : ""}`}>
-                    <div className='w-12 rounded-full'>
-                        <img
-                            src={conversation.profilePic}
-                            alt='user pic'
-                        />
-                    </div>
+                <div className={`relative w-12 h-12 ${isOnline ? "after:content-[''] after:absolute after:w-3 after:h-3 after:bg-green-400 after:border-2 after:border-white after:rounded-full after:right-0 after:bottom-0" : ""}`}>
+                    {/* Gray background with pulse animation */}
+                    {isLoading && (
+                        <div className="absolute inset-0 bg-gray-300 rounded-full animate-pulse" />
+                    )}
+                    <img
+                        src={conversation.profilePic}
+                        alt='user pic'
+                        className={`w-full h-full object-cover rounded-full transition-opacity duration-300 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
+                        onLoad={handleImageLoad}
+                        onError={(e) => e.target.src = 'path/to/placeholder-image.png'} // Fallback image
+                    />
                 </div>
 
                 <div className='flex flex-col flex-1'>
@@ -36,4 +47,4 @@ const Conversation = ({ conversation, emoji, lastIndex, }) => {
     )
 }
 
-export default Conversation
+export default Conversation;
